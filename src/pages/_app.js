8e64ae64bcf3c10cce2d8e293c9dc0ad6485791e
Kanishk Khurana <kanishkkhurana152@gmail.com>
2023-02-16 18:20:29 +0530

    finish

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import '@/styles/globals.css'
 import ModeProvider from '../context/ModeContext'
-
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function App({ Component, pageProps }) {
   return (
